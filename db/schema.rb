a27ef5f61367c260c74dddecda3b5198bda68e68
Rob Brown <brown4homes@yahoo.com>
2018-12-26 09:15:13 -0800

    add one to many association between recipe and chef

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_001244) do
+ActiveRecord::Schema.define(version: 2018_12_26_164431) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_12_22_001244) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
 end
