4cf725e8d3e95330cbcf693b0a92ee60b4283b5e
Rob Brown <brown4homes@yahoo.com>
2019-01-08 10:19:39 -0800

    Edit action with test

---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+ def setup
+    @chef = Chef.create!(chefname: "rob", email: "rob@example.com",
+                        password: "password", password_confirmation: "password")
+  end
+
+  test "reject an invalid edit" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: " ", email: "rob@example.com " } }
+    assert_template 'chefs/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test "accept vaild signup" do 
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: {chef: { chefname: "rob1 ", email: "rob1@example.com " } }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "rob1", @chef.chefname
+    assert_match "rob1@example.com", @chef.email
+  end
+end
