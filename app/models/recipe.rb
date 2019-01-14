0a881203ea352b4e7cba28b96878aaa5f3984570
Rob Brown <brown4homes@yahoo.com>
2019-01-14 09:38:41 -0800

    Add ingredient model and assoc with recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :chef
   validates :name, presence: true
-  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 600 }
   validates :chef_id, presence: true
   default_scope -> { order(updated_at: :desc) }
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
