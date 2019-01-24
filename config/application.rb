f6d6b026ddd51f7ad532b5c0917f287845ea5169
Rob Brown <brown4homes@yahoo.com>
2019-01-24 15:39:56 -0800

    carrierwave file back to original code

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,5 @@ module Recipes
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_cable.mount_path = '/cable'
-    config.assets.initialize_on_precompile = false
   end
 end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,15 +1,10 @@
-CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_provider = 'fog/aws'
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory = ENV['S3_BUCKET']
-    config.storage = :fog
-  else
-    config.storage = :file
-    config.enable_processing = Rails.env.development?
   end
 end
